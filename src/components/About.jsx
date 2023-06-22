304f99ce9d0084eb9015500aa427524f9d74891f
Lemon-tech24 <alisangcocarl@gmail.com>
2023-06-22 16:47:11 +0800

    Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,27 @@
 import Navigation from './Navigation';
 
-
 function About() {
+  const images = ['html.png', 'css.png', 'js.png', 'node.png','php.png', 'python.png','react.png','tailwind.png'];
+  
   return (
     <div>
-        <Navigation/>
-        <div className="aboutContent">
+      <Navigation/>
+      <div className="aboutContent">
+        <div className="description">
+          <p>Welcome to my Personal Portfolio! This is Carl Hari Alisangco. You can call me by my given name. Currently studying and pursuing a Bachelor of Science in Information Technology. Inspiring FullStack Developer. Busy honing my skills to prepare for my career. Wish me luck :D.</p>
+        </div>
+        <div className="techs">
+          <p>Technologies I used: </p>
+          
+          <div className="images">
+              {images.map((imageList, index) => (<img key={index} src={imageList} alt={imageList} />))}
+          </div>
           
         </div>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
