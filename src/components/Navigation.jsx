2d231ed16c2d8e0a6a3f6cf1478dce6a83db60c2
Lemon-tech24 <alisangcocarl@gmail.com>
2023-06-29 00:50:15 +0800

    title icon, adjust contact sizes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,14 +5,14 @@ function Navigation() {
 
   return (
    <>
-<div className="nav" >
-    <div><Link to="/" className="title">Carl Hari</Link></div>
-    <div className="menuBtn">
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <div className="nav" >
+        <div><Link to="/" className="title">Carl Hari</Link></div>
+        <div className="menuBtn">
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </div>
     </div>
-</div>
    </>
   )
 }
